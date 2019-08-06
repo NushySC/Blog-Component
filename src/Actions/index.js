ba949996dff049e472344f26c123cda19d998a48
Anuska <ank.sampedro@gmail.com>
2019-08-06 20:32:00 +0200

    memoize

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,7 +1,25 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jasonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
-		const response = await jsonPlaceholder.get('/posts');
+	const response = await jsonPlaceholder.get('/posts');
 
-		dispatch({type: 'FETCH_POSTS', payload: response.data});
-	};
+	dispatch({type: 'FETCH_POSTS', payload: response.data});
+};
+
+// export const fetchUser = (id) => async dispatch => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+// 	dispatch({type: 'FETCH_USER', payload: response.data});
+// 	//console.log(response.data)
+// };
+
+export const fetchUser = id => dispatch => {
+	_fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({type: 'FETCH_USER', payload: response.data});
+});
